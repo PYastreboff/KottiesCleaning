963853a54c3e5b9b8dacaa920a9e976aca81758e
PY <70672659+PYastreboff@users.noreply.github.com>
2023-08-28 18:27:20 +1000

    added faqs

--- js/site.js
+++ js/site.js
@@ -65,3 +65,35 @@ $(window).load(function () { // makes sure the whole site is loaded
 	}
 
 });
+
+// Accordion
+// ========================================================================
+
+const accordionItemHeaders = document.querySelectorAll(
+	".accordion-item-header"
+  );
+  
+  accordionItemHeaders.forEach((accordionItemHeader) => {
+	accordionItemHeader.addEventListener("click", (event) => {
+	  // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+  
+	  const currentlyActiveAccordionItemHeader = document.querySelector(
+		".accordion-item-header.active"
+	  );
+	  if (
+		currentlyActiveAccordionItemHeader &&
+		currentlyActiveAccordionItemHeader !== accordionItemHeader
+	  ) {
+		currentlyActiveAccordionItemHeader.classList.toggle("active");
+		currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+	  }
+	  accordionItemHeader.classList.toggle("active");
+	  const accordionItemBody = accordionItemHeader.nextElementSibling;
+	  if (accordionItemHeader.classList.contains("active")) {
+		accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+	  } else {
+		accordionItemBody.style.maxHeight = 0;
+	  }
+	});
+  });
+  
